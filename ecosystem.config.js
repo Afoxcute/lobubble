356834b118857c3decbbe8588db62010bb416018
Afoxcute <jwavolabilove16@gmail.com>
2025-04-15 13:27:19 +0100

    fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,21 +6,8 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
     env_production: {
       NODE_ENV: 'production'
-    },
-    log_date_format: 'YYYY-MM-DD HH:mm:ss',
-    error_file: 'logs/error.log',
-    out_file: 'logs/output.log',
-    merge_logs: true,
-    // Ensure restart in case of errors
-    max_restarts: 10,
-    restart_delay: 5000,
-    // Enable graceful shutdown
-    kill_timeout: 5000,
-    wait_ready: true
+    }
   }]
 }; 
